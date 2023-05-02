df93b540c63db44c56e697f1e1d574022d69d855
Duceman56 <114949617+Duceman56@users.noreply.github.com>
2023-05-02 15:38:18 -0500

    Update 2019GunArchiveData.r

--- 2019GunArchiveData.r
+++ 2019GunArchiveData.r
@@ -2,7 +2,7 @@ library(readr)
 library(dplyr)
 
 ##data importation
-handgun11 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataDec2nd2019ThroughDec31st2019HandgunIncidents.csv")
+handgun1 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataDec2nd2019ThroughDec31st2019HandgunIncidents.csv")
 rifle <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJan1st2019ThroughDec31st2019RifleIncidents.csv")
 handgun2 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJan1st2019ThroughMar1st2019HandgunIncidents.csv")
 handgun3 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJul2nd2019ThroughSep1st2019HandgunIncidents.csv")
@@ -10,6 +10,15 @@ handgun4 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/mai
 handgun5 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataMay2nd2019ThroughJul1st2019HandgunIncidents.csv")
 handgun6 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataSep2nd2019ThroughDec1st2019HandgunIncidents.csv")
 legalDefenseByState <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/usDataOnLegalityOfDefense.csv")
+handgun106 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataSep2nd2019ThroughDec1st2019HandgunIncidents.csv")
+handgun103 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJul2nd2019ThroughSep1st2019HandgunIncidents.csv")
+View(handgun3)
+
+# select only wanted columns
+handgun3 <- subset(handgun3, select = c(Address, State))
+
+# combine the handgun dataframes into one large dataframe
+combinedTest <- rbind(handgun106, handgun103)
 
 ## combine the individual address, state/county, and state into one field
 tempDf <- paste(handgun11$Address, handgun11$`City Or County`, handgun11$State, sep = ", ")
