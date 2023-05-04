c6f0a49ae63dca0839467a64dfe862ee625d4afb
Duceman56 <114949617+Duceman56@users.noreply.github.com>
2023-05-04 11:38:39 -0500

    Update 2019GunArchiveData.r

--- 2019GunArchiveData.r
+++ 2019GunArchiveData.r
@@ -4,7 +4,6 @@ library(tmaptools)
 
 # citations
 
-
 ##data importation
 handgun1 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataDec2nd2019ThroughDec31st2019HandgunIncidents.csv")
 rifle <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJan1st2019ThroughDec31st2019RifleIncidents.csv")
@@ -25,13 +24,34 @@ handgun6 <- handgun6[, 1:7]
 
 # combine the handgun data frames into one large data frame
 handgun <- rbind(handgun1, handgun2, handgun3, handgun4, handgun5, handgun6)
+wiHandgun <- subset(handgun, handgun$State == "Wisconsin")
+
 
 ## combine the individual address, state/county, and state into one field
-wholeAddress <- paste(handgun$Address, handgun$`City Or County`, handgun$State, sep = ", ")
-handgunTemp <- as.data.frame(wholeAddress)
+wholeAddress <- paste(handgun$Address, handgun$`City Or County`, handgun$State, sep = ", ") ## for whole handgun data set
+handgunTemp <- as.data.frame(wholeAddress) ## for whole handgun data set
 
-wholeAddress <- paste(handgun1$Address, handgun1$`City Or County`, handgun1$State, sep = ", ")
-handgunTemp <- as.data.frame(wholeAddress)
+wholeAddressWi <- paste(wiHandgun$Address, wiHandgun$`City Or County`, wiHandgun$State, sep = ", ") ## for wisconsin handgun data set
+handgunWi <- as.data.frame(wholeAddressWi) ## for wisconsin handgun data set
+geocode_OSM(handgunWi$wholeAddressWi)
+
+wholeAddress1 <- paste(handgun1$Address, handgun1$`City Or County`, handgun1$State, sep = ", ")
+handgunTemp1 <- as.data.frame(wholeAddress1)
+
+wholeAddress2 <- paste(handgun2$Address, handgun2$`City Or County`, handgun2$State, sep = ", ")
+handgunTemp2 <- as.data.frame(wholeAddress2)
+
+wholeAddress3 <- paste(handgun3$Address, handgun3$`City Or County`, handgun3$State, sep = ", ")
+handgunTemp3 <- as.data.frame(wholeAddress3)
+
+wholeAddress4 <- paste(handgun4$Address, handgun4$`City Or County`, handgun4$State, sep = ", ")
+handgunTemp4 <- as.data.frame(wholeAddress4)
+
+wholeAddress5 <- paste(handgun5$Address, handgun5$`City Or County`, handgun5$State, sep = ", ")
+handgunTemp5 <- as.data.frame(wholeAddress5)
+
+wholeAddress6 <- paste(handgun6$Address, handgun6$`City Or County`, handgun6$State, sep = ", ")
+handgunTemp6 <- as.data.frame(wholeAddress6)
 
 ## add address to main handgun dataframe
 handgun <- cbind(handgun, handgunTemp)
