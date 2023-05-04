19e5acf746c62c4945561849576d282631089c70
Duceman56 <114949617+Duceman56@users.noreply.github.com>
2023-05-03 21:44:55 -0500

    Update 2019GunArchiveData.r

--- 2019GunArchiveData.r
+++ 2019GunArchiveData.r
@@ -1,33 +1,41 @@
 library(readr)
 library(dplyr)
+library(tmaptools)
+
+# citations
+
 
 ##data importation
-handgun1 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataDec2nd2019ThroughDec31st2019HandgunIncidents.csv", col_names = T, comment = "")
-rifle <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJan1st2019ThroughDec31st2019RifleIncidents.csv", col_names = T, comment = "")
-handgun2 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJan1st2019ThroughMar1st2019HandgunIncidents.csv", col_names = T, comment = "")
-handgun3 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJul2nd2019ThroughSep1st2019HandgunIncidents.csv", col_names = T, comment = "")
-handgun4 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataMar2nd2019ThroughMay1st2019HandgunIncidents.csv", col_names = T, comment = "")
-handgun5 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataMay2nd2019ThroughJul1st2019HandgunIncidents.csv", col_names = T, comment = "")
-handgun6 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataSep2nd2019ThroughDec1st2019HandgunIncidents.csv", col_names = T, comment = "")
-legalDefenseByState <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/usDataOnLegalityOfDefense.csv", col_names = T, comment = "")
-
-handgun106 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataSep2nd2019ThroughDec1st2019HandgunIncidents.csv", col_names = T, comment = "")
-handgun103 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJul2nd2019ThroughSep1st2019HandgunIncidents.csv", col_names = T, comment = "")
-
-View(handgun3)
+handgun1 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataDec2nd2019ThroughDec31st2019HandgunIncidents.csv")
+rifle <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJan1st2019ThroughDec31st2019RifleIncidents.csv")
+handgun2 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJan1st2019ThroughMar1st2019HandgunIncidents.csv")
+handgun3 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataJul2nd2019ThroughSep1st2019HandgunIncidents.csv")
+handgun4 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataMar2nd2019ThroughMay1st2019HandgunIncidents.csv")
+handgun5 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataMay2nd2019ThroughJul1st2019HandgunIncidents.csv")
+handgun6 <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/gunArchiveDataSep2nd2019ThroughDec1st2019HandgunIncidents.csv")
+legalDefenseByState <- read_csv("https://raw.githubusercontent.com/Duceman56/PublicData/main/usDataOnLegalityOfDefense.csv")
 
 # select only wanted columns
-handgun3 <- subset(handgun3, select = c(Address, State, "City Or County", "# Killed", "# Injured", "Incident ID", "Incident Date"))
-handgun3 <- subset(handgun3, select = c(Address, State, City Or County))
+handgun1 <- handgun1[, 1:7]
+handgun2 <- handgun2[, 1:7]
+handgun3 <- handgun3[, 1:7]
+handgun4 <- handgun4[, 1:7]
+handgun5<- handgun5[, 1:7]
+handgun6 <- handgun6[, 1:7]
 
-?subset
-?read_csv
 # combine the handgun data frames into one large data frame
-combinedTest <- rbind(handgun106, handgun103)
+handgun <- rbind(handgun1, handgun2, handgun3, handgun4, handgun5, handgun6)
 
 ## combine the individual address, state/county, and state into one field
-Address <- paste(handgun106$Address, handgun106$`City Or County`, handgun106$State, sep = ", ")
-handgun106 <- as.data.frame(Address)  
+wholeAddress <- paste(handgun$Address, handgun$`City Or County`, handgun$State, sep = ", ")
+handgunTemp <- as.data.frame(wholeAddress)
 
-View(handgun106)
+wholeAddress <- paste(handgun1$Address, handgun1$`City Or County`, handgun1$State, sep = ", ")
+handgunTemp <- as.data.frame(wholeAddress)
+
+## add address to main handgun dataframe
+handgun <- cbind(handgun, handgunTemp)
+
+## geocode portion
+geocoded <- geocode_OSM(handgunTemp$wholeAddress)
 
